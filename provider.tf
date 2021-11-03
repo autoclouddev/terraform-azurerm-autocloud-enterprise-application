e8778049cb0739bb08f676e2449e4554cda1d018
Kevin Loyola <kevin.loyola@autocloud.dev>
2021-11-03 07:07:40 -0600

    Add Azure Enterprise app tf module

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "*****"
+  client_id   = "*****"
+  client_secret = "*****"
+  name        = "*****"
+  tenant_id     = "*****"
+}
+
---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.7.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.82.0"
+    }
+  }
+}
